c390151aaf18ea1104573d2d7bf9fa7528913d57
xtaodada <xtao@xtaolink.cn>
2023-06-08 22:25:28 +0800

    update style

--- plugins/starrail/rogue.py
+++ plugins/starrail/rogue.py
@@ -1,4 +1,4 @@
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Tuple
 
 from genshin import Client, GenshinException
 from genshin.models import StarRailRogue, RogueCharacter
@@ -39,14 +39,16 @@ class PlayerRoguePlugins(Plugin):
         self.assets = assets
         self.helper = helper
 
-    async def get_uid(self, user_id: int, args: List[str], reply: Optional[Message]) -> int:
+    async def get_uid(self, user_id: int, args: List[str], reply: Optional[Message]) -> Tuple[int, bool]:
         """通过消息获取 uid，优先级：args > reply > self"""
-        uid, user_id_ = None, user_id
+        uid, user_id_, pre = None, user_id, False
         if args:
             for i in args:
                 if i is not None:
                     if i.isdigit() and len(i) == 9:
                         uid = int(i)
+                    if "上" in i:
+                        pre = True
         if reply:
             try:
                 user_id_ = reply.from_user.id
@@ -60,7 +62,7 @@ class PlayerRoguePlugins(Plugin):
                 player_info = await self.helper.players_service.get_player(user_id)
                 if player_info is not None:
                     uid = player_info.player_id
-        return uid
+        return uid, pre
 
     @handler.command("rogue", block=False)
     @handler.message(filters.Regex("^模拟宇宙信息查询(.*)"), block=False)
@@ -69,7 +71,7 @@ class PlayerRoguePlugins(Plugin):
         message = update.effective_message
         logger.info("用户 %s[%s] 查询模拟宇宙信息命令请求", user.full_name, user.id)
         try:
-            uid: int = await self.get_uid(user.id, context.args, message.reply_to_message)
+            uid, pre = await self.get_uid(user.id, context.args, message.reply_to_message)
             if uid and str(uid)[0] not in ["1", "2", "5"]:
                 # todo: 支持国际服
                 raise NotSupport
@@ -79,7 +81,7 @@ class PlayerRoguePlugins(Plugin):
                     raise CookiesNotFoundError
             except CookiesNotFoundError:
                 client, _ = await self.helper.get_public_genshin_client(user.id)
-            render_result = await self.render(client, uid)
+            render_result = await self.render(client, pre, uid)
         except PlayerNotFoundError:
             buttons = [
                 [InlineKeyboardButton("点我绑定账号", url=create_deep_linked_url(context.bot.username, "set_cookie"))]]
@@ -121,7 +123,7 @@ class PlayerRoguePlugins(Plugin):
         await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
         await render_result.reply_photo(message, filename=f"{client.uid}.png", allow_sending_without_reply=True)
 
-    async def get_rander_data(self, uid: int, data: StarRailRogue) -> Dict:
+    async def get_rander_data(self, uid: int, data: StarRailRogue, pre: bool) -> Dict:
         luo_ma_bum = ["", "Ⅰ", "Ⅱ", "Ⅲ", "Ⅳ", "Ⅴ", "Ⅵ"]
         buff_en_map = {
             "「丰饶」": "Abundance",
@@ -134,18 +136,22 @@ class PlayerRoguePlugins(Plugin):
             "「虚无」": "Nihility",
             "「存护」": "Preservation",
         }
-        record = data.current_record.records[0]
+        record_raw = data.last_record if pre else data.current_record
+        if not record_raw.has_data:
+            raise NotHaveData
+        record = record_raw.records[0]
         avatars = record.final_lineup
-        new_avatars = []
-        for avatar in avatars:
+        new_avatars = [None, None, None, None]
+        for idx, avatar in enumerate(avatars):
             old_avatar = avatar.dict()
             old_avatar["icon"] = self.assets.avatar.square(avatar.id).as_uri()
-            new_avatars.append(RogueCharacter(**old_avatar))
+            new_avatars[idx] = RogueCharacter(**old_avatar)
 
         return {
             "uid": uid,
             "basic": data.basic_info,
             "name": f"{record.name} {luo_ma_bum[record.difficulty]}",
+            "finish_cnt": record_raw.basic.finish_cnt,
             "time": record.finish_time.datetime.strftime("%Y-%m-%d %H:%M"),
             "score": record.score,
             "avatars": new_avatars,
@@ -154,19 +160,17 @@ class PlayerRoguePlugins(Plugin):
             "miracles": record.miracles,
         }
 
-    async def render(self, client: Client, uid: Optional[int] = None) -> RenderResult:
+    async def render(self, client: Client, pre: bool, uid: Optional[int] = None) -> RenderResult:
         if uid is None:
             uid = client.uid
 
         rogue = await client.get_starrail_rogue(uid)
-        if not rogue.current_record.has_data:
-            raise NotHaveData
-        data = await self.get_rander_data(uid, rogue)
+        data = await self.get_rander_data(uid, rogue, pre)
 
         return await self.template_service.render(
             "starrail/rogue/rogue.html",
             data,
-            {"width": 400, "height": 1000},
+            {"width": 520, "height": 1000},
             full_page=True,
             query_selector="#new-container",
         )
