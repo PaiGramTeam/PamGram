e3a500b30c60b51b9a34dfd04d1049900b6077c4
xtaodada <xtao@xtaolink.cn>
2023-06-16 20:17:15 +0800

    add help

--- plugins/account/devices.py
+++ plugins/account/devices.py
@@ -1,6 +1,5 @@
-from typing import Dict, Optional
+from typing import Optional
 
-import ujson
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, TelegramObject, Update
 from telegram.ext import CallbackContext, ConversationHandler, filters
 from telegram.helpers import escape_markdown
@@ -11,9 +10,8 @@ from core.services.cookies.services import CookiesService
 from core.services.devices import DevicesService
 from core.services.devices.models import DevicesDataBase as Devices
 from core.services.players.services import PlayersService
-from utils.log import logger
-
 from modules.apihelper.utility.devices import devices_methods
+from utils.log import logger
 
 __all__ = ("AccountDevicesPlugin",)
 
@@ -49,25 +47,32 @@ class AccountDevicesPlugin(Plugin.Conversation):
         devices_methods.service = devices_service
 
     @staticmethod
-    def parse_cookie(data: AccountDevicesPluginData, cookie: Dict[str, str]) -> None:
-        if not isinstance(cookie, dict):
-            raise ValueError
+    def parse_headers(data: AccountDevicesPluginData, headers_text: str) -> None:
+        headers = {}
+        for line in headers_text.splitlines():
+            if not line:
+                continue
+            try:
+                k, v = line.split(":", 1)
+                headers[k.strip()] = v.strip()
+            except ValueError:
+                continue
         must_keys = {"x-rpc-device_id": 36, "x-rpc-device_fp": 13}
         optional_keys = ["x-rpc-device_name"]
         for k, v in must_keys.items():
-            if (k not in cookie) or (not cookie.get(k)):
+            if (k not in headers) or (not headers.get(k)):
                 raise ValueError
-            if len(cookie.get(k)) != v:
+            if len(headers.get(k)) != v:
                 raise ValueError
         for k in optional_keys:
-            if k not in cookie:
+            if k not in headers:
                 continue
-            elif cookie.get(k) and len(cookie.get(k)) > 64:
+            elif headers.get(k) and len(headers.get(k)) > 64:
                 raise ValueError
 
-        data.device_id = cookie.get("x-rpc-device_id")
-        data.device_fp = cookie.get("x-rpc-device_fp")
-        data.device_name = cookie.get("x-rpc-device_name")
+        data.device_id = headers.get("x-rpc-device_id")
+        data.device_fp = headers.get("x-rpc-device_fp")
+        data.device_name = headers.get("x-rpc-device_name")
 
     @conversation.entry_point
     @handler.command(command="setdevice", filters=filters.ChatType.PRIVATE, block=False)
@@ -113,7 +118,19 @@ class AccountDevicesPlugin(Plugin.Conversation):
         else:
             await message.reply_text(f"你还没有绑定 {bbs_name} 的Cookies，请先绑定Cookies")
             return ConversationHandler.END
-        await message.reply_text(f"请输入{bbs_name}的 JSON 格式的设备 ID 以及对应 FP ！或回复退出取消操作", reply_markup=ReplyKeyboardRemove())
+        help_message = (
+            "<b>关于如何获取Device</b>\n"
+            "<b>此操作只能在PC上进行。</b>\n\n"
+            "PC：\n"
+            "1、打开<a href='https://user.mihoyo.com/'>通行证</a>并登录\n"
+            "2、进入通行证按F12打开开发者工具\n"
+            "3、将开发者工具切换至网络(Network)并点击过滤栏中的 Fetch/XHR 并刷新页面\n"
+            "4、在请求列表中找到 login_by_cookie\n"
+            "5、右键并复制请求标头(Request Headers)\n"
+            "<u>如发现没有此请求大概因为缓存的存在需要你点击禁用缓存(Disable Cache)再次刷新页面</u>"
+        )
+        await message.reply_html(help_message, disable_web_page_preview=True)
+        await message.reply_text(f"请输入{bbs_name}的请求标头！或回复退出取消操作", reply_markup=ReplyKeyboardRemove())
         return INPUT_DEVICES
 
     @conversation.state(state=INPUT_DEVICES)
@@ -126,14 +143,16 @@ class AccountDevicesPlugin(Plugin.Conversation):
             await message.reply_text("退出任务", reply_markup=ReplyKeyboardRemove())
             return ConversationHandler.END
         try:
-            self.parse_cookie(account_devices_plugin_data, ujson.loads(message.text))
-        except (ValueError, ujson.JSONDecodeError) as exc:
+            self.parse_headers(account_devices_plugin_data, message.text)
+        except ValueError as exc:
             logger.info("用户 %s[%s] Devices解析出现错误\ntext:%s", user.full_name, user.id, message.text)
             logger.debug("解析Devices出现错误", exc_info=exc)
             await message.reply_text("解析Devices出现错误，请检查是否正确", reply_markup=ReplyKeyboardRemove())
             return ConversationHandler.END
         reply_keyboard = [["确认", "退出"]]
-        await message.reply_markdown_v2("请确认修改！", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        await message.reply_markdown_v2(
+            "请确认修改！", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        )
         return COMMAND_RESULT
 
     @conversation.state(state=COMMAND_RESULT)
