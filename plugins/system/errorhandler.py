afd53bf57b34fc2930d91891d965798eb5ec8bd1
xtaodada <xtao@xtaolink.cn>
2023-07-19 19:46:19 +0800

    remove aiohttp

--- plugins/system/errorhandler.py
+++ plugins/system/errorhandler.py
@@ -4,7 +4,6 @@ import traceback
 from typing import Optional
 
 import aiofiles
-from aiohttp import ClientError, ClientConnectorError
 from httpx import HTTPError, TimeoutException
 from simnet.errors import (
     DataNotPublic,
@@ -30,7 +29,6 @@ from modules.errorpush import (
     SentryClientException,
 )
 from utils.log import logger
-from utils.patch.aiohttp import AioHttpTimeoutException
 
 try:
     import ujson as jsonlib
@@ -216,20 +214,6 @@ class ErrorHandler(Plugin):
         if isinstance(exc, TimeoutException):
             notice = self.ERROR_MSG_PREFIX + " 服务器熟啦 ~ 请稍后再试"
             logger.warning("Httpx exception[%s]", str(exc))
-        if notice:
-            self.create_notice_task(update, context, notice)
-            raise ApplicationHandlerStop
-
-    @error_handler()
-    async def process_aiohttp_exception(self, update: object, context: CallbackContext):
-        if not isinstance(context.error, ClientError) or not isinstance(update, Update):
-            return
-        exc = context.error
-        notice: Optional[str] = None
-        if isinstance(exc, AioHttpTimeoutException):
-            notice = self.ERROR_MSG_PREFIX + " 服务器熟啦 ~ 请稍后再试"
-        elif isinstance(exc, ClientConnectorError):
-            notice = self.ERROR_MSG_PREFIX + " 连接服务器异常"
         if notice:
             self.create_notice_task(update, context, notice)
             raise ApplicationHandlerStop
