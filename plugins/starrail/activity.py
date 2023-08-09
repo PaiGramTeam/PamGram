ddccf1118d38682911deb43683396c95ada0b3bb
xtaodada <xtao@xtaolink.cn>
2023-08-09 18:10:18 +0800

    :sparkles: Support starrail activity - TreasureDungeon

--- plugins/starrail/activity.py
+++ plugins/starrail/activity.py
@@ -1,6 +1,11 @@
-from typing import Optional, List, Dict, TYPE_CHECKING
+import asyncio
+from typing import Optional, List, Dict, TYPE_CHECKING, Any, Coroutine
 
-from simnet.models.starrail.chronicle.activity import StarRailStarFight, StarRailFantasticStory
+from simnet.models.starrail.chronicle.activity import (
+    StarRailStarFight,
+    StarRailFantasticStory,
+    StarRailTreasureDungeonRecord,
+)
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Message
 from telegram.constants import ChatAction
 from telegram.ext import CallbackContext, filters
@@ -8,8 +13,8 @@ from telegram.helpers import create_deep_linked_url
 
 from core.dependence.assets import AssetsService
 from core.plugin import Plugin, handler
-from core.services.template.models import RenderResult
 from core.services.template.services import TemplateService
+from gram_core.services.template.models import RenderResult, RenderGroupResult
 from plugins.tools.genshin import GenshinHelper, PlayerNotFoundError, CookiesNotFoundError
 from utils.log import logger
 
@@ -197,3 +202,81 @@ class PlayerActivityPlugins(Plugin):
             full_page=True,
             query_selector="#container",
         )
+
+    @handler.command("treasure_dungeon", block=False)
+    @handler.message(filters.Regex("^地城探宝信息查询(.*)"), block=False)
+    async def treasure_dungeon_command_start(self, update: Update, context: CallbackContext) -> Optional[int]:
+        user = update.effective_user
+        message = update.effective_message
+        logger.info("用户 %s[%s] 查询地城探宝信息命令请求", user.full_name, user.id)
+        try:
+            uid = await self.get_uid(user.id, context.args, message.reply_to_message)
+            async with self.helper.genshin(user.id) as client:
+                render_result = await self.treasure_dungeon_render(client, uid)
+                if render_result is None:
+                    raise NotHaveData
+        except PlayerNotFoundError:
+            buttons = [[InlineKeyboardButton("点我绑定账号", url=create_deep_linked_url(context.bot.username, "set_cookie"))]]
+            if filters.ChatType.GROUPS.filter(message):
+                reply_message = await message.reply_text(
+                    "未查询到您所绑定的账号信息，请先私聊彦卿绑定账号", reply_markup=InlineKeyboardMarkup(buttons)
+                )
+                self.add_delete_message_job(reply_message, delay=30)
+                self.add_delete_message_job(message, delay=30)
+            else:
+                await message.reply_text("未查询到您所绑定的账号信息，请先绑定账号", reply_markup=InlineKeyboardMarkup(buttons))
+            return
+        except AttributeError as exc:
+            logger.error("活动数据有误")
+            logger.exception(exc)
+            await message.reply_text("活动数据有误 估计是彦卿晕了")
+            return
+        except NotHaveData:
+            reply_message = await message.reply_text("没有查找到此活动数据")
+            if filters.ChatType.GROUPS.filter(reply_message):
+                self.add_delete_message_job(message)
+                self.add_delete_message_job(reply_message)
+            return
+        await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
+        await RenderGroupResult(results=render_result).reply_media_group(
+            message, allow_sending_without_reply=True, write_timeout=60
+        )
+        logger.info("用户 %s[%s] [bold]地城探宝信息数据[/bold]: 成功发送图片", user.full_name, user.id, extra={"markup": True})
+
+    async def treasure_dungeon_render(self, client: "StarRailClient", uid: Optional[int] = None) -> List[RenderResult]:
+        if uid is None:
+            uid = client.player_id
+
+        act_data = await client.get_starrail_activity(uid)
+        try:
+            data = act_data.treasure_dungeon
+            if not data.exists_data:
+                raise NotHaveData
+            if all(record.finish_time is None for record in data.records):
+                raise NotHaveData
+        except ValueError:
+            raise NotHaveData
+
+        avatar_icons = {}
+        for record in data.records:
+            for avatar in record.avatars:
+                avatar_icons[avatar.id] = self.assets.avatar.icon(avatar.id).as_uri()
+
+        def render(record: StarRailTreasureDungeonRecord) -> Coroutine[Any, Any, RenderResult]:
+            render_data = {
+                "uid": uid,
+                "record": record,
+                "avatar_icons": avatar_icons,
+            }
+            return self.template_service.render(
+                "starrail/activity/treasure_dungeon.html",
+                render_data,
+                {"width": 500, "height": 762},
+                full_page=True,
+                query_selector="#container",
+            )
+
+        tasks = [render(record) for record in data.records if record.finish_time is not None]
+        if len(tasks) == 1:
+            return [await tasks[0]]
+        return await asyncio.gather(*tasks)
