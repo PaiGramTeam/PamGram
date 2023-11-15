a39ad6a2492e9092bfeb5a9809b3190eadcfc00f
xtaodada <xtao@xtaolink.cn>
2023-11-15 23:43:58 +0800

    :sparkles: Support starrail activity - FoxStory

--- plugins/starrail/activity.py
+++ plugins/starrail/activity.py
@@ -1,9 +1,11 @@
 import asyncio
-from typing import Optional, List, Dict, TYPE_CHECKING, Any, Coroutine
+import math
+from typing import Optional, List, Dict, TYPE_CHECKING, Any, Coroutine, Tuple
 
 from simnet.models.starrail.chronicle.activity import (
     StarRailFantasticStory,
     StarRailTreasureDungeonRecord,
+    StarRailFoxStoryTeam,
 )
 from telegram import Update, Message
 from telegram.constants import ChatAction
@@ -226,6 +228,352 @@ class PlayerActivityPlugins(Plugin):
             {"width": 960, "height": 1000},
             full_page=True,
             query_selector="#endless_side",
+        )
+
+    @handler.command("fox_story", block=False)
+    @handler.message(filters.Regex("^狐斋志异信息查询(.*)"), block=False)
+    async def fox_story_command_start(self, update: Update, context: CallbackContext) -> Optional[int]:
+        user = update.effective_user
+        message = update.effective_message
+        logger.info("用户 %s[%s] 查询狐斋志异信息命令请求", user.full_name, user.id)
+        try:
+            uid = await self.get_uid(user.id, context.args, message.reply_to_message)
+            async with self.helper.genshin_or_public(user.id, uid=uid) as client:
+                render_result = await self.fox_story_render(client, uid)
+        except AttributeError as exc:
+            logger.error(ACTIVITY_DATA_ERROR)
+            logger.exception(exc)
+            await message.reply_text(ACTIVITY_ATTR_ERROR)
+            return
+        except NotHaveData as e:
+            reply_message = await message.reply_text(e.MSG)
+            if filters.ChatType.GROUPS.filter(reply_message):
+                self.add_delete_message_job(message)
+                self.add_delete_message_job(reply_message)
+            return
+        await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
+        await render_result.reply_photo(message, filename=f"{user.id}.png", allow_sending_without_reply=True)
+
+    @staticmethod
+    def line_percent(team: StarRailFoxStoryTeam) -> Tuple[float, float]:
+        if team.index_arrived == 0:
+            return 0, 0
+        x, i = 0.0, team.index_arrived
+        if i > 0:
+            a = i - 1
+            n = team.fans_range[i - 1]
+            r = (team.fans - n) / (team.fans_range[i] - n)
+            l = a + 0.5
+            x = max(0.0, min(1.0, (r + l) * (2 / (2 * len(team.fans_range) - 1))))
+        d = [
+            2.5,
+            3,
+            4,
+            4.5,
+            5,
+            5.5,
+            6,
+            7,
+            7,
+            8,
+            8.5,
+            9,
+            9.5,
+            10,
+            11,
+            11.5,
+            12,
+            13,
+            13,
+            14,
+            14.5,
+            15,
+            16,
+            16.5,
+            17,
+            18,
+            19,
+            20,
+            20.5,
+            21,
+            21.5,
+            22,
+            22.5,
+            22.5,
+            22.5,
+            23.5,
+            24.5,
+            25,
+            26.5,
+            27.5,
+            28.5,
+            29,
+            30,
+            31,
+            31.5,
+            32,
+            32.5,
+            32.5,
+            32.5,
+            33.5,
+            33.5,
+            33.5,
+            33.5,
+            33.5,
+            33.5,
+            33.5,
+            34,
+            35,
+            35.5,
+            36,
+            37,
+            37.5,
+            38,
+            38.5,
+            38.5,
+            38.5,
+            38.5,
+            37.5,
+            37.5,
+            37.5,
+            36.5,
+            37,
+            37,
+            38,
+            39,
+            39,
+            40,
+            40.5,
+            41,
+            42,
+            42.5,
+            43,
+            43.5,
+            43.5,
+            44.5,
+            44.5,
+            44.5,
+            45,
+            45.5,
+            45.5,
+            46,
+            47,
+            47,
+            48,
+            48.5,
+            49,
+            50,
+            50.5,
+            51,
+            52,
+            52.5,
+            53,
+            53.5,
+            53.5,
+            53.5,
+            53.5,
+            54,
+            54.5,
+            55.5,
+            56.5,
+            58,
+            59.5,
+            60.5,
+            61.5,
+            62,
+            63,
+            63.5,
+            64,
+            65,
+            65.5,
+            66,
+            66.5,
+            66.5,
+            66.5,
+            66.5,
+            66.5,
+            66.5,
+            67,
+            67,
+            68,
+            69,
+            70,
+            70,
+            70.5,
+            71.5,
+            71.5,
+            71.5,
+            71.5,
+            71.5,
+            71.5,
+            71.5,
+            72.5,
+            72.5,
+            72.5,
+            73,
+            73.5,
+            73.5,
+            74,
+            74.5,
+            75,
+            76,
+            77,
+            77.5,
+            78.5,
+            79,
+            80,
+            80.5,
+            81,
+            81.5,
+            81.5,
+            81.5,
+            81.5,
+            81.5,
+            81.5,
+            81.5,
+            81.5,
+            81.5,
+            81.5,
+            81,
+            80.5,
+            80.5,
+            81,
+            81.5,
+            82,
+            83,
+            83.5,
+            84,
+            84.5,
+            85,
+            86,
+            86.5,
+            87.5,
+            88.5,
+            89.5,
+            90.5,
+            92,
+            93,
+            94.5,
+            95.5,
+            96.5,
+            97.5,
+            98.5,
+            99.5,
+            100.5,
+            101.5,
+            102.5,
+            103.5,
+            104.5,
+            105.5,
+            106.5,
+            108,
+            109,
+            110.5,
+            111.5,
+            112.5,
+            113.5,
+            114.5,
+            115.5,
+            116.5,
+            117,
+            118,
+            118.5,
+            118.5,
+            119,
+            119.5,
+            120,
+            120.5,
+            121.5,
+            122.5,
+            123.5,
+            124.5,
+            126,
+            127,
+            128,
+            129.5,
+            130.5,
+            131.5,
+            132.5,
+            133.5,
+            134.5,
+            135.5,
+            136,
+            137,
+            138,
+            138,
+            139,
+            139.5,
+            139.5,
+            140.5,
+            140.5,
+            141,
+            141.5,
+            142,
+            143,
+            143.5,
+            143.5,
+            144.5,
+            144.5,
+            145,
+            145.5,
+            145.5,
+            146,
+            146.5,
+            147,
+            147.5,
+            148,
+            148.5,
+            149,
+            149.5,
+            150,
+            150.5,
+            151,
+            152,
+            153,
+            153.5,
+            154.5,
+            155.5,
+            156.5,
+            158,
+            158.5,
+        ]
+
+        def v(t):
+            e = max(0, int(t))
+            if e == len(d):
+                return 0
+            i = min(int(math.ceil(t)), len(d) - 1)
+            s = d[e]
+            return (s + (d[i] - s) * (t - e)) / 162
+
+        return x * 100, v(x * len(d)) * 100
+
+    async def fox_story_render(self, client: "StarRailClient", uid: Optional[int] = None) -> RenderResult:
+        if uid is None:
+            uid = client.player_id
+
+        act_data = await client.get_starrail_activity(uid)
+        try:
+            fox_story_data = act_data.fox_story
+            if not fox_story_data.exists_data:
+                raise NotHaveData
+            fox_story_data = fox_story_data.info
+            if not fox_story_data.exists_data:
+                raise NotHaveData
+        except ValueError:
+            raise NotHaveData
+        data = {
+            "uid": mask_number(uid),
+            "data": fox_story_data,
+            "line": self.line_percent(fox_story_data.team),
+        }
+
+        return await self.template_service.render(
+            "starrail/activity/fox_story.html",
+            data,
+            {"width": 1920, "height": 1000},
+            full_page=True,
+            query_selector="#fox_story",
         )
 
     @handler.command("treasure_dungeon", block=False)
