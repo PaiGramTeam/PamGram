1f93dabe1ca1925700a20e72ac5b8d82b69c94ac
xtaodada <xtao@xtaolink.cn>
2024-02-06 23:09:41 +0800

    add callback

--- plugins/starrail/role_detail.py
+++ plugins/starrail/role_detail.py
@@ -6,10 +6,11 @@ from simnet.models.starrail.chronicle.characters import StarRailDetailCharacters
     RecommendProperty
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ChatAction
-from telegram.ext import CallbackContext, filters
+from telegram.ext import CallbackContext, filters, ContextTypes
 
 from core.plugin import Plugin, handler
 from core.services.template.services import TemplateService
+from gram_core.config import config
 from gram_core.dependence.redisdb import RedisDB
 from gram_core.services.template.models import RenderResult
 from metadata.shortname import roleToName, idToRole
@@ -22,6 +23,10 @@ if TYPE_CHECKING:
 
 
 __all__ = ("RoleDetailPlugin",)
+
+
+class NeedClient(Exception):
+    """无缓存，需要 StarRailClient"""
 
 
 class RelicScoreData(BaseModel, frozen=False):
@@ -103,9 +108,11 @@ class RoleDetailPlugin(Plugin):
         json_data = str(data_v, encoding="utf-8")
         return nickname, StarRailDetailCharacters.parse_raw(json_data)
 
-    async def get_characters(self, client: "StarRailClient") -> Tuple[Optional[str], Optional["StarRailDetailCharacters"]]:
-        nickname, data = await self.get_characters_for_redis(client.player_id)
+    async def get_characters(self, uid: int, client: "StarRailClient" = None) -> Tuple[Optional[str], Optional["StarRailDetailCharacters"]]:
+        nickname, data = await self.get_characters_for_redis(uid)
         if nickname is None or data is None:
+            if not client:
+                raise NeedClient
             data = await client.get_starrail_characters()
             nickname = (await client.get_starrail_user()).info.nickname
             await self.set_characters_for_redis(client.player_id, nickname, data)
@@ -278,7 +285,7 @@ class RoleDetailPlugin(Plugin):
         )
 
     @handler.command(command="role_detail", block=False)
-    async def command_start(self, update: Update, context: CallbackContext) -> None:
+    async def command_start(self, update: Update, context: "ContextTypes.DEFAULT_TYPE") -> None:
         user = update.effective_user
         message = update.effective_message
         args = self.get_args(context)
@@ -294,7 +301,7 @@ class RoleDetailPlugin(Plugin):
             ch_name,
         )
         async with self.helper.genshin(user.id) as client:
-            nickname, data = await self.get_characters(client)
+            nickname, data = await self.get_characters(client.player_id, client)
         uid = client.player_id
         if ch_name is None:
             buttons = self.gen_button(data, user.id, uid)
@@ -319,3 +326,75 @@ class RoleDetailPlugin(Plugin):
         render_result = await self.get_render_result(data, nickname, ch_name, client.player_id)
         await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
         await render_result.reply_photo(message, filename=f"{client.player_id}.png", allow_sending_without_reply=True)
+
+    @handler.callback_query(pattern=r"^get_role_detail\|", block=False)
+    async def get_role_detail(self, update: "Update", _: "ContextTypes.DEFAULT_TYPE") -> None:
+        callback_query = update.callback_query
+        user = callback_query.from_user
+        message = callback_query.message
+
+        async def get_role_detail_callback(
+            callback_query_data: str,
+        ) -> Tuple[str, int, int]:
+            _data = callback_query_data.split("|")
+            _user_id = int(_data[1])
+            _uid = int(_data[2])
+            _result = _data[3]
+            logger.debug(
+                "callback_query_data函数返回 result[%s] user_id[%s] uid[%s]",
+                _result,
+                _user_id,
+                _uid,
+            )
+            return _result, _user_id, _uid
+
+        result, user_id, uid = await get_role_detail_callback(callback_query.data)
+        if user.id != user_id:
+            await callback_query.answer(text="这不是你的按钮！\n" + config.notice.user_mismatch, show_alert=True)
+            return
+        if result == "empty_data":
+            await callback_query.answer(text="此按钮不可用", show_alert=True)
+            return
+        page = 0
+        if result.isdigit():
+            page = int(result)
+            logger.info(
+                "用户 %s[%s] 角色详细信息查询命令请求 || page[%s] uid[%s]",
+                user.full_name,
+                user.id,
+                page,
+                uid,
+            )
+        else:
+            logger.info(
+                "用户 %s[%s] 角色详细信息查询命令请求 || character_name[%s] uid[%s]",
+                user.full_name,
+                user.id,
+                result,
+                uid,
+            )
+        try:
+            nickname, data = await self.get_characters(uid)
+        except NeedClient:
+            async with self.helper.genshin(user.id) as client:
+                nickname, data = await self.get_characters(client.player_id, client)
+        if page:
+            buttons = self.gen_button(data, user.id, uid, page)
+            await message.edit_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
+            await callback_query.answer(f"已切换到第 {page} 页", show_alert=False)
+            return
+        for characters in data.avatar_list:
+            if idToRole(characters.id) == result:
+                break
+        else:
+            await message.delete()
+            await callback_query.answer(
+                f"未在游戏中找到 {result} ，请检查角色是否存在，或者等待角色数据更新后重试",
+                show_alert=True,
+            )
+            return
+        await callback_query.answer(text="正在渲染图片中 请稍等 请不要重复点击按钮", show_alert=False)
+        await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
+        render_result = await self.get_render_result(data, nickname, result, uid)
+        render_result.filename = f"role_detail_{uid}_{result}.png"
+        await render_result.edit_media(message)
