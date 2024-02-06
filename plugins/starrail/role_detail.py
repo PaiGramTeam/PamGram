90c33ed0210baff70feef41e64820802b5c48635
xtaodada <xtao@xtaolink.cn>
2024-02-06 21:45:31 +0800

    fix equip empty

--- plugins/starrail/role_detail.py
+++ plugins/starrail/role_detail.py
@@ -1,15 +1,17 @@
 import os
-from typing import TYPE_CHECKING, Dict, Any, List
+from typing import TYPE_CHECKING, Dict, Any, List, Tuple
 
 from pydantic import BaseModel
-from simnet.models.starrail.chronicle.characters import StarRailDetailCharacters, PropertyInfo
+from simnet.models.starrail.chronicle.characters import StarRailDetailCharacters, PropertyInfo, StarRailDetailCharacter, \
+    RecommendProperty
 from telegram import Update
 from telegram.constants import ChatAction
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, filters
 
 from core.plugin import Plugin, handler
 from core.services.cookies import CookiesService
 from core.services.template.services import TemplateService
+from metadata.shortname import roleToName
 from plugins.tools.genshin import GenshinHelper
 from utils.log import logger
 from utils.uid import mask_number
@@ -83,12 +85,10 @@ class RoleDetailPlugin(Plugin):
         return properties_map
 
     @staticmethod
-    def process_property(data: "StarRailDetailCharacters", index: int, score: RelicScore) -> List[List[Dict[str, Any]]]:
+    def process_property(properties_map: Dict[int, "PropertyInfo"], char: "StarRailDetailCharacter", score: RelicScore) -> List[List[Dict[str, Any]]]:
         """处理角色属性"""
-        char = data.avatar_list[index].properties
-        properties_map = RoleDetailPlugin.get_properties_map(data)
         data = []
-        for i in char:
+        for i in char.properties:
             info = properties_map[i.property_type]
             new_data = i.dict()
             new_data["show_add"] = i.show_add
@@ -105,19 +105,16 @@ class RoleDetailPlugin(Plugin):
         return data2
 
     @staticmethod
-    def process_relics(data: "StarRailDetailCharacters", index: int, score: RelicScore) -> List[Dict[str, Any]]:
+    def process_relics(properties_map: Dict[int, "PropertyInfo"], char: "StarRailDetailCharacter", score: RelicScore) -> List[Dict[str, Any]]:
         """处理角色遗物"""
-        properties_map = RoleDetailPlugin.get_properties_map(data)
-
         def process_relic_prop(_data: Dict[str, Any]) -> None:
             info = properties_map[_data["property_type"]]
             _data["highlight"] = info.property_type in score.ids
             _data["name"] = info.property_name_relic
             _data["icon"] = info.icon
 
-        relics = data.avatar_list[index].relics
-        ornaments = data.avatar_list[index].ornaments
-        properties_map = RoleDetailPlugin.get_properties_map(data)
+        relics = char.relics
+        ornaments = char.ornaments
         data_map: Dict[int, Dict[str, Any]] = {}
         for i in (relics + ornaments):
             new_data = i.dict()
@@ -137,23 +134,24 @@ class RoleDetailPlugin(Plugin):
         return [i[1] for i in data_map1]
 
     @staticmethod
-    def process_char(data: "StarRailDetailCharacters", index: int) -> Dict[str, Any]:
-        char = data.avatar_list[index]
-        data = char.dict()
-        data["path"] = RoleDetailPlugin.BASE_TYPE_EN[char.base_type]
-        data["skills_map"] = [[j.dict() for j in i] for i in char.skills_map]
-        data["skills_main"] = [i.dict() for i in char.skills_main]
-        data["skills_single"] = [i.dict() for i in char.skills_single]
-        return data
+    def process_char(data: "StarRailDetailCharacters", ch_name: str) -> Tuple["StarRailDetailCharacter", Dict[str, Any]]:
+        for char in data.avatar_list:
+            if char.name != ch_name:
+                continue
+            data = char.dict()
+            data["path"] = RoleDetailPlugin.BASE_TYPE_EN[char.base_type]
+            data["skills_map"] = [[j.dict() for j in i] for i in char.skills_map]
+            data["skills_main"] = [i.dict() for i in char.skills_main]
+            data["skills_single"] = [i.dict() for i in char.skills_single]
+            return char, data
+        raise ValueError("未找到角色")
 
     @staticmethod
-    def process_score(data: "StarRailDetailCharacters", char_id: int) -> RelicScore:
-        properties_map = RoleDetailPlugin.get_properties_map(data)
-        info = data.get_recommend_property_by_cid(char_id)
-        if info.is_custom_property_valid:
-            ids = info.custom_relic_properties
+    def process_score(properties_map: Dict[int, "PropertyInfo"], recommend_property: "RecommendProperty") -> RelicScore:
+        if recommend_property.is_custom_property_valid:
+            ids = recommend_property.custom_relic_properties
         else:
-            ids = info.recommend_relic_properties
+            ids = recommend_property.recommend_relic_properties
         return RelicScore(
             ids=ids,
             count=0,
@@ -166,33 +164,49 @@ class RoleDetailPlugin(Plugin):
                     property_name_relic=properties_map[i].property_name_relic,
                 ) for i in ids
             ],
-            is_custom=info.is_custom_property_valid,
+            is_custom=recommend_property.is_custom_property_valid,
         )
+
+    def parse_render_data(self, data: "StarRailDetailCharacters", nickname: str, ch_name: str, uid: int):
+        properties_map = RoleDetailPlugin.get_properties_map(data)
+        char, char_data = self.process_char(data, ch_name)
+        recommend_property = data.get_recommend_property_by_cid(char.id)
+        score = self.process_score(properties_map, recommend_property)
+        properties = self.process_property(properties_map, char, score)
+        relics = self.process_relics(properties_map, char, score)
+        return {
+            "nickname": nickname,
+            "uid": mask_number(uid),
+            "char": char_data,
+            "properties": properties,
+            "relics": relics,
+            "score": score.dict(),
+        }
 
     @handler.command(command="role_detail", block=False)
     async def command_start(self, update: Update, context: CallbackContext) -> None:
         user = update.effective_user
         message = update.effective_message
-
-        logger.info("用户 %s[%s] 查询角色详细信息", user.full_name, user.id)
+        args = self.get_args(context)
+        ch_name = None
+        for i in args:
+            ch_name = roleToName(i)
+            if ch_name:
+                break
+        logger.info("用户 %s[%s] 查询角色详细信息 ch_name[%s]", user.full_name, user.id, ch_name)
         await message.reply_chat_action(ChatAction.TYPING)
         async with self.helper.genshin(user.id) as client:
             client: "StarRailClient"
             info = (await client.get_starrail_user()).info
             data = await client.get_starrail_characters()
-            index = 3
-            char = self.process_char(data, index)
-            score = self.process_score(data, char["id"])
-            properties = self.process_property(data, index, score)
-            relics = self.process_relics(data, index, score)
-            final = {
-                "nickname": info.nickname,
-                "uid": mask_number(client.player_id),
-                "char": char,
-                "properties": properties,
-                "relics": relics,
-                "score": score.dict(),
-            }
+        try:
+            final = self.parse_render_data(data, info.nickname, ch_name, client.player_id)
+        except ValueError:
+            reply_message = await message.reply_text("未找到该角色")
+            if filters.ChatType.GROUPS.filter(reply_message):
+                self.add_delete_message_job(message)
+                self.add_delete_message_job(reply_message)
+            return
         await message.reply_chat_action(ChatAction.UPLOAD_PHOTO)
         render_result = await self.template_service.render(
             "starrail/role_detail/main.jinja2",
