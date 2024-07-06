8610fb5b6343dfec8d8bd7da87fad306cc5b8237
xtaodada <xtao@xtaolink.cn>
2024-07-06 18:33:54 +0800

    :arrow_up: Update playwright requirement from 1.44.0 to 1.45.0

--- plugins/account/account.py
+++ plugins/account/account.py
@@ -76,7 +76,7 @@ class BindAccountPlugin(Plugin.Conversation):
             bind_account_plugin_data.reset()
         text = (
             f"你好 {user.mention_markdown_v2()} "
-            f'{escape_markdown("！请输入通行证ID（非游戏玩家ID），BOT将会通过通行证UID查找游戏UID。请选择要绑定的服务器！或回复退出取消操作")}'
+            f'{escape_markdown("！请选择要绑定的服务器！或回复退出取消操作")}'
         )
         reply_keyboard = [["米游社", "HoYoLab"], ["退出"]]
         await message.reply_markdown_v2(text, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
