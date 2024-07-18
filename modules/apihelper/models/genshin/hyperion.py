f6174f64c91ad6ae1fe333b48b9c60701b1bb2aa
xtaodada <xtao@xtaolink.cn>
2024-07-18 22:40:42 +0800

    :bug: Fix post get official recommended posts

--- modules/apihelper/models/genshin/hyperion.py
+++ modules/apihelper/models/genshin/hyperion.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from enum import Enum
 from io import BytesIO
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Dict
 
 from PIL import Image, UnidentifiedImageError
 from pydantic import BaseModel, PrivateAttr
@@ -167,7 +167,9 @@ class HoYoPostMultiLang(BaseModel):
 class PostRecommend(BaseModel):
     hoyolab: bool = False
     post_id: int
-    subject: str
-    banner: Optional[str] = None
-    official_type: Optional[int] = None
-    multi_language_info: Optional[HoYoPostMultiLang] = None
+
+    @staticmethod
+    def parse(data: Dict, hoyolab: bool = False):
+        _post = data.get("post")
+        post_id = _post.get("post_id")
+        return PostRecommend(hoyolab=hoyolab, post_id=post_id)
