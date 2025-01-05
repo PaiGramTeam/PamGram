f4481ed8384f85dce8ad81f7871f8ecae8ac5b5c
xtaodada <xtao@xtaolink.cn>
2025-01-05 22:28:30 +0800

    :arrow_up: Update python-telegram-bot requirement to 21.10

--- modules/action_log/date.py
+++ modules/action_log/date.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 
-from pytz import timezone
+from zoneinfo import ZoneInfo
 
-TZ = timezone("Asia/Shanghai")
+TZ = ZoneInfo("Asia/Shanghai")
 
 
 class DateUtils:
