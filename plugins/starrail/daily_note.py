d8824a33c91de11a695c8d8eda88e8aefe9fcf82
xtaodada <xtao@xtaolink.cn>
2025-02-13 14:30:00 +0800

    :sparkles: Add starrail notes rogue tourn exp is full

--- plugins/starrail/daily_note.py
+++ plugins/starrail/daily_note.py
@@ -77,6 +77,7 @@ class DailyNotePlugin(Plugin):
             "rogue_tourn_weekly_unlocked": daily_info.rogue_tourn_weekly_unlocked,
             "rogue_tourn_weekly_max": daily_info.rogue_tourn_weekly_max,
             "rogue_tourn_weekly_cur": daily_info.rogue_tourn_weekly_cur,
+            "rogue_tourn_exp_is_full": daily_info.rogue_tourn_exp_is_full,
         }
         render_result = await self.template_service.render(
             "starrail/daily_note/daily_note.html",
