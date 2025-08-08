1e2cf82c0287e5db5514ce9e4b7d5a1c9e5933d8
xtaodada <xtao@xtao.de>
2025-08-08 15:26:02 +0800

    :clock1: Update daily sign job time to 00:11

--- plugins/jobs/sign.py
+++ plugins/jobs/sign.py
@@ -14,7 +14,7 @@ class SignJob(Plugin):
     def __init__(self, sign_system: SignSystem):
         self.sign_system = sign_system
 
-    @job.run_daily(time=datetime.time(hour=0, minute=1, second=0), name="SignJob")
+    @job.run_daily(time=datetime.time(hour=0, minute=11, second=0), name="SignJob")
     async def sign(self, context: "ContextTypes.DEFAULT_TYPE"):
         logger.info("正在执行自动签到")
         await self.sign_system.do_sign_job(context, job_type=SignJobType.START)
